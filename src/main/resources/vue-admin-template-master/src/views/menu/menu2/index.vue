9b6e2cfe85d01f2e393adb27d3cb5fc1927762a3
LuJifeng <1342739070@qq.com>
2019-06-27 22:04:31 +0800

    精简代码后

--- src/main/resources/vue-admin-template-master/src/views/menu/menu2/index.vue
+++ src/main/resources/vue-admin-template-master/src/views/menu/menu2/index.vue
@@ -1,41 +1,6 @@
 <template>
   <div style="padding:30px;">
 
-    <div class="filter-container">
-      <el-input
-        v-model="listQuery.title"
-        placeholder="Title"
-        style="width: 200px;"
-        class="filter-item"
-        @keyup.enter.native="handleFilter"
-      />
-
-      <el-button
-        v-waves
-        class="filter-item"
-        type="primary"
-        icon="el-icon-search"
-        @click="handleFilter"
-      >Search</el-button>
-      <el-button
-        class="filter-item"
-        style="margin-left: 10px;"
-        type="primary"
-        icon="el-icon-edit"
-        @click="handleCreate"
-      >Add</el-button>
-    </div>
-
-
-    <el-button
-      class="filter-item"
-      style="margin-left: 10px;"
-      type="primary"
-      icon="el-icon-edit"
-      @click="handleCreate"
-    >Add</el-button>
-
-
 
 
 
---
+++ src/main/resources/vue-admin-template-master/src/views/menu/menu2/menu2-2/demand_table.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "demand_table"
+    }
+</script>
+
+<style scoped>
+
+</style>
