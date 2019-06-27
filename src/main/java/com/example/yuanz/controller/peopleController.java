42ee2715a4851ec41a6ed5ca3fa9e02c8cecdb23
Creeper <gggg@gggg.kkk>
2019-06-27 16:50:34 +0800

    version 1.0

--- src/main/java/com/example/yuanz/controller/peopleController.java
+++ src/main/java/com/example/yuanz/controller/peopleController.java
@@ -5,16 +5,7 @@ public class peopleController {
     private String peopletrun(){
         String a="asd";
         return a;
-        shorts
-                shortsss
-                s
-                        s
-                        s
-                                s
-                                s
-                                        s1s
 
-                                        s1s
     }
 
 }
