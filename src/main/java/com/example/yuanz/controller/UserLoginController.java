e509d1cafc5aa210490200e16f5d9bd410c595ab
Creeper <gggg@gggg.kkk>
2019-06-30 20:54:59 +0800

    version 1.9

--- src/main/java/com/example/yuanz/controller/UserLoginController.java
+++ src/main/java/com/example/yuanz/controller/UserLoginController.java
@@ -1,7 +1,9 @@
 package com.example.yuanz.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.yuanz.entity.PowerEditEntity;
 import com.example.yuanz.server.Administratorlpml;
+import com.example.yuanz.server.Powerlmpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Controller
@@ -17,7 +20,8 @@ import java.util.UUID;
 public class UserLoginController {
     @Autowired
     Administratorlpml administratorlpml;
-
+    @Autowired
+    Powerlmpl powerlmpl;
 
     @ResponseBody
     @RequestMapping("/user/login")
@@ -58,7 +62,7 @@ public class UserLoginController {
         data.put("introduction", "I am a super administrator");
         data.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
         data.put("name", name);
-     //   System.out.println(name);
+        //   System.out.println(name);
         object.put("data", data);
 
 
@@ -77,5 +81,55 @@ public class UserLoginController {
         return object;
     }
 
+
+    @ResponseBody
+    @RequestMapping("/userRouter")
+    public ArrayList<String> router(HttpServletRequest request) {
+        String name = null;
+        Cookie[] cookies = request.getCookies();
+        if (null != cookies) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("username")) {
+                    name = cookie.getValue();
+                }
+            }
+        }
+        ArrayList<String> arrayList = new ArrayList<>();
+        if (powerlmpl.findById(name).isPresent()) {
+            PowerEditEntity powerEditEntity = new PowerEditEntity();
+            powerEditEntity = powerlmpl.findById(name).get();
+
+            if (powerEditEntity.getRoadEdit().equals(1)) {
+                arrayList.add("road_edit");
+            }
+            if (powerEditEntity.getHelppeopleEdit().equals(1)) {
+                arrayList.add("helppeople_edit");
+            }
+            if (powerEditEntity.getHelpZEdit().equals(1)) {
+                arrayList.add("helpZ_edit");
+            }
+            if (powerEditEntity.getNeedEdit().equals(1)) {
+                arrayList.add("need_edit");
+            }
+            if (powerEditEntity.getPersonalEdit().equals(1)) {
+                arrayList.add("personal_edit");
+            }
+            if (powerEditEntity.getRoleEdit().equals(1)) {
+                arrayList.add("role_edit");
+            }
+            if (powerEditEntity.getSchoolEdit().equals(1)) {
+                arrayList.add("school_edit");
+            }
+            if (powerEditEntity.getWorkdataEdit().equals(1)) {
+                arrayList.add("workdata_edit");
+            }
+            if (powerEditEntity.getPowerEdit().equals(1)) {
+                arrayList.add("power_edit");
+            }
+        }
+
+
+        return arrayList;
+    }
 
 }
